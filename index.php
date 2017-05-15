55a3126e4dae8fb31c82bd1d07f313d7967a3520
13539919834@163.com <13539919834@163.com>
2017-05-15 17:30:42 +0800

    66665

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+	$url = "https://github.com/13539919834/lizhicheng.git";
+	// $filename = 'index.html';
+	$beizhu = 'beizhu';
+	// copy('./abc/'.$filename,'./lizhicheng/dp.html');
+	// exec('cd D:\phpStudy\WWW\githubBlogAdmin');
+	// exec('git clone index.html');
+	// exec('cd D:\phpStudy\WWW\githubBlogAdmin\lizhicheng',$a);
+	// exec('cd D:\phpStudy\WWW\githubBlogAdmin\lizhicheng');
+	// exec('git init');
+	exec('git add .');
+	exec('git commit -m "66665"');
+	exec('git push');
+
+	// var_dump($d1);
